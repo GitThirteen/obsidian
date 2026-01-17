2416493157ecc60804a2340ccea019e33f710636
bitThirteen <mich1.eickmeyer@gmail.com>
2026-01-17 12:26:35 +0100

    remove todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,5 @@
 #include <native/core/obsidian.h>
 
-/* !==== Neverending List of ToDo's ====! */
-// [THU] [ ] Implement lighting (Ambient, Directional, Point, for now..)
-// [THU] [ ] Environment map support
-// [THU] [ ] GGX Terrance-Cook PBR
-// [FRI] [ ] Bloom
-// [FRI] [ ] Compute shader particle system (rain)
-// [FRI] [ ] LoD + Accel Octree
-// [SAT] [ ] Ray-Traced Shadows
-// [SUN] [ ] Texture check
-// [SUN] [ ] Camera check
-// [SUN] [ ] Build scene
-// Later:
-// [ ] Water ripples
-// [ ] Audio manager
-// [ ] Ray-traced reflections
-// [ ] Modular shader code (Allow toggling effects)
-// ===============================================================
-// [ ] 2x/4x MSAA / TAA & more...
-// [ ] Avoid write when compiling from GLSL to SPIR-V
-// [ ] Add audio support
-// [ ] Improve GUI
-// [ ] Fix swapchain recreation on window resize
-// [ ] Swap all AVK calls with regular VK calls (own abstraction - Magma, AVK too inconsistent)
-// [ ] Refactor like, idk, half the renderer
-// [ ] -> Use C++ modules instead of headers
-// [ ] Physics? (Colliders, Actors, Gravity, ...)
-// [ ] Raytracing!! (For everything) Path Tracing? (Path Guiding...?)
-/* !====================================! */
-
 int main(int c, char* v[])
 {
 	try
